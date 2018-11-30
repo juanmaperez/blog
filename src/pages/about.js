b30ec5b0d2cec361497abd2b1277a570d17e5c77
juanmaperez <juanmaperezvar@gmail.com>
2018-11-30 22:16:55 +0000

    animation game

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,28 +4,44 @@ import Layout from '../components/layout'
 
 class AboutPage extends Component {
 
-  
+  state = {
+    status: null, 
+  }
+
+  changeValueClose = () => {
+    let { status } = this.state;
+    if(status === null) {
+      status = 'open';
+    } else {
+      status = status === 'open' ? 'close' : 'open';
+    }
+    this.setState(()=> ({
+      status
+    }))
+  }
   
   render(){
-    let close = 'close';
-    function changeValueClose() {
-      console.log('hey');
-      close = close === 'close' ? '' : 'close';
-    }
-
+    const { status } = this.state;
     return (
       <Layout>
-        
-        <div className="main-page about">
-          <div className="content {{close}}">
-            <span>Here goes the content for about</span><br/>
-            <button onClick={changeValueClose()}>btn</button>
+        {/* <div className="main-page about">
+          <div className={'chocolate ' + close}>
+            <span>Here goes the left content for about</span><br/>
+            <button onClick={this.changeValueClose}>btn</button>
 
           </div>
-          <div className="aside {{close}}">
-            <span>Here goes the content for about</span><br/>
-            <button onClick={changeValueClose()}>btn</button>
-
+          <div className={'teal ' + close}>
+            <span>Here goes the right content for about</span><br/>
+            <button onClick={this.changeValueClose}>btn</button>
+          </div>
+        </div> */}
+          <div className="main-page about">
+          <button onClick={this.changeValueClose}>btn</button>
+          <div className={'chocolate ' + status}>
+            <span className='span-content'>Here goes the left content for about</span>
+          </div>
+          <div className={'teal ' + status}>
+            <span className='span-content'>Here goes the right content for about</span>
           </div>
         </div>
       </Layout>
