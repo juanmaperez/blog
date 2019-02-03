c2c0fb007533523cce8fbaba35855ffafe4cd2ed
juanmaperez <juanmaperezvar@gmail.com>
2019-02-03 12:31:11 +0000

    less resolve some margin

--- src/components/techDisplay.js
+++ src/components/techDisplay.js
@@ -2,44 +2,39 @@ import React from 'react';
 import styled from 'styled-components'
 import Image from './image';
 
+function calculateRotation(index){
+	const angle = 360 / 9;
+	const rot = angle * index;
+	return rot
+}
+
+function divideByTwo (amount){
+	return amount/2
+}
+
+
 const TechDisplayContainer = styled.div`
-	@mixin on-circle($item-count, $circle-size, $item-size) {
-		position: relative;
-		width:  $circle-size;
-		height: $circle-size;
-		padding: 0;
-		border-radius: 50%; 
-		list-style: none;       
-			
-		.tech-icon {
-			display: block;
-			position: absolute;
-			top:  50%; 
-			left: 50%;
-			width:  $item-size;
-			height: $item-size;
-			margin: -($item-size / 2);
-			
-			$angle: (360 / $item-count);
-			$rot: 0;
-	
-			@for $i from 1 through $item-count {
-				&:nth-of-type(#{$i}) {
-				transform: 
-						rotate($rot * 1deg) 
-						translate($circle-size / 2) 
-						rotate($rot * -1deg);
-				}
-
-				$rot: $rot + $angle;
-			}
-		}
-	}
-
 	.techs {
-		@include on-circle($item-count: 9, $circle-size: ${(props) => props.size}, $item-size: 200px); 
-		margin: 5em auto 0;
-		
+		border-radius: 50%;
+		height: ${props => props.size }px;
+		width: ${props => props.size }px;
+		position: relative
+	}
+` 
+
+
+const TechIcon = styled.div`
+		display: block;
+		z-index: 2;
+		width: 100px;
+		height:100px;
+		overflow: hidden;
+		border-radius: 50%;
+		position:absolute;
+		top: ${props => (divideByTwo(props.size)-50) }px;
+		left: ${props => (divideByTwo(props.size)-50) }px; 
+		transform: rotate(${props => calculateRotation(props.index)}deg) translate(${props => divideByTwo(props.size)}px) rotate(-${props => calculateRotation(props.index)}deg);
+
 		img { 
 			display: block; 
 			max-width: 100%; 
@@ -52,15 +47,15 @@ const TechDisplayContainer = styled.div`
 				filter: grayscale(0);
 			}
 		}
-	}
-` 
+	`
+
 class TechDisplay extends React.Component {
 	render() {
 		const techs = ['react', 'angular', 'gatsby', 'css3', 'html5', 'wordpress', 'node', 'js', 'github']
 		return (
 			<TechDisplayContainer {...this.props}>
 				{ this.props.complete && <div className="techs">
-					{ techs.map((icon)=> <div key={icon} className={`tech-icon ${icon}`} ><Image src={`/icons/${icon}.png`}/></div>)}
+					{ techs.map((icon, index)=> <TechIcon {...this.props} index={index}><div key={icon} className={`tech-icon ${icon}`} ><Image src={`/icons/${icon}.png`}/></div></TechIcon>)}
 				</div> }
 			</TechDisplayContainer>
 		)
