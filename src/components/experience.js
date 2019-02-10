95fbe858559c4899dde4b4700b8bc54fbe817582
juanmaperez <juanmaperezvar@gmail.com>
2019-02-10 19:31:27 +0000

    changes in about page

---
+++ src/components/experience.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from './image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquare } from '@fortawesome/free-solid-svg-icons'
+
+const Experience = (props) => {
+  const { node } = props;
+
+  return (
+  <div className={'experience-item ' + (node ? 'complete' : '')}>
+    <div className="experience-header">
+      <Image name={node.title} src={`/icons/${node.icon}`} />
+      <div>
+        <h3 className="title">{ node.title }</h3>
+        <time className="date">{ node.date }</time>
+      </div>
+    </div>
+    <ul className="tasks">
+      {node.tasks.map((task, index) => <li className="task" key={index}><FontAwesomeIcon icon={faSquare} /> &nbsp; &nbsp;{task}</li> )}
+    </ul>
+  </div>
+)}
+
+export default Experience
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,60 @@
 import React, { Component} from 'react'
+import Experience from './../components/experience';
 
-import './../styles/index.scss'
+import './../styles/about.scss'
+
 
 class AboutPage extends Component {
-
+  experiences = [
+    {
+      title: "Colossus Bets",
+      date: "June 2018",
+      icon: "colossus.jpg",
+      tasks: [
+        "Work focused on getting the best user experience on the main application.",
+        "Continued improvement of the main view of the application working close to the design team.",
+        "Apply the best logic to manage the different information displayed on the view.",
+        "Develop different services to set the application state and persist information.",
+        "Git workflow in every environment."]
+    },
+    {
+      title: "Crealogix",
+      date: "November 2017 | June 2018",
+      icon: "crealogix.png",
+      tasks: [
+        "Bring to the website the instructions related to the app features and functionalities in each report.",
+        "Modules develop based on Angular 2, 4, 5 for fintech applications.",
+        "Manage data flowing between different screens through customized Angular Router.",
+        "Communication between front and back through different components and services and manage JSON responses.",
+        "Show information for users finding the best solution through a decision-making process based on UX.",
+        "TeamWork in development based on Control Version Git Flow."
+      ]
+    },
+    {
+      title: "InboundCycle",
+      date: "November 2015 | November 2017",
+      icon: "inboundcycle.jpg",
+      tasks: [
+        "Landing pages, Thank you pages, emails design and development from scratch with HTML5, SASS and JS.",
+        "Exclusive WordPress Themes development and marketing tools integration.",
+        "UX design application to increase visitors conversion to leads.",
+        "Internal tools developed to increase productivity.",
+        "Projects development based in SCRUM methodologies.",
+        "RESTful API integrations and source control with git.",
+        "Experience with Client Side frameworks as Angular2."
+      ]
+    }
+  ]
 
   render(){
     return (
         <div id="about" className="page">
           <div className="title-container">
             <h1>Who I used to be</h1>
-          </div>        
+          </div> 
+          <div className="about-container">
+            {this.experiences.map((experience, index)=> <Experience key={index} node={experience}/>)}
+          </div>      
         </div>
      )
   }
