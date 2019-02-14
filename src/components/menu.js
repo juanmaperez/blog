13716a96e834acbd808f48bab5427fb62f5cb0fc
juanmaperez <juanmaperezvar@gmail.com>
2019-02-14 19:38:50 +0000

    menu changed

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,116 @@
 import React, { Component } from 'react';
 import { Link } from 'gatsby'
+import styled from 'styled-components'
 import { navigate } from 'gatsby-link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown, faAngleUp, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+
+const MenuView = styled.div`
+.menu {
+  position: relative;
+
+  .goUp {
+    color:#fff;
+    cursor: pointer;
+    position: absolute;
+    top:0px;
+    left: 10px;
+    width: 30px;
+    text-align: center;
+    margin-left:-15px;
+    z-index: 3;
+    opacity: 1;
+    @media(max-width:480px){
+      top: auto;
+      bottom:20px;
+      left: 10px;
+    }
+    i { font-size: 30px;}
+    &:hover {
+      opacity: .7
+    }
+  }
+  .goDown {
+    color:#fff;
+    cursor: pointer;
+    position: absolute;
+    bottom: 0px;
+    left: 10px;
+    width: 30px;
+    text-align: center;
+    margin-left:-15px;
+    z-index: 15;
+    opacity: 1;
+    i { font-size: 30px; }
+    &:hover {
+      opacity: .7
+    }
+  }
+  .menu-wrapper {
+    min-height: 100%;
+    height: 300px;
+    position: relative;
+    // comment to test
+    overflow: hidden;
+
+    @media(max-width: 480px){
+      height: 150px;
+
+    }
+ 
+    ul.menu-container {
+      position:relative;
+      transition: top 600ms linear;
+
+      .menu-link {
+        cursor: pointer;
+        height: 500px;
+        padding-left: 0px;
+        padding-left: 0px;
+        @media(max-width:480px){
+          height: 150px; 
+          padding-left: 30px;
+          line-height: 150px;
+        }
+
+        .link { 
+          display:inline-block;
+          font-weight: 200;
+          font-size: 400px;
+          line-height: 300px;
+          text-decoration: none;
+          position: relative;
+          color:#fff;
+          z-index: 2;
+          height: 300px;
+          font-family: 'MFred' !important;
+          text-transform: uppercase;
+         @media(max-width:480px){
+           font-size: 132px;
+           height: 150px; 
+           line-height: 100px;
+
+         }
+          .icon {
+            font-size: 45px;
+            line-height: 0;
+            transform: rotate(45deg);
+            color: #fff;
+            position: relative;
+            top: 3px;
+            opacity:0;
+            transition: 200ms ease;
+          }
+          &:hover{
+            .icon {opacity: 1}
+          }
+          
+        }
+      }
+    }
+  }
+}
+`
 
 class Menu extends Component {
   scrollTimer = null;
@@ -78,15 +186,17 @@ class Menu extends Component {
     const styles = { top: -(height * active)}
     
     return (
-      <div className="menu">
-        { active < (options.length -1) &&  <span className="goUp" onClick={this.goUp}><FontAwesomeIcon icon={faAngleUp} /></span>}
-        { active > 0 && <span className="goDown" onClick={this.goDown}><FontAwesomeIcon icon={faAngleDown} /></span>}
-        <div className="menu-wrapper">
-          <ul style={ styles } className="menu-container">
-            { options.map((option) => <li key={option.title} className="menu-link"><Link to={ option.to }/><span onClick={this.props.handleContent} className="link">{ option.title }<FontAwesomeIcon className="icon" icon={faArrowUp} /></span></li> )}  
-          </ul>
+      <MenuView>
+        <div className="menu">
+          { active < (options.length -1) &&  <span className="goUp" onClick={this.goUp}><FontAwesomeIcon icon={faAngleUp} /></span>}
+          { active > 0 && <span className="goDown" onClick={this.goDown}><FontAwesomeIcon icon={faAngleDown} /></span>}
+          <div className="menu-wrapper">
+            <ul style={ styles } className="menu-container">
+              { options.map((option) => <li key={option.title} className="menu-link"><Link to={ option.to }/><span onClick={this.props.handleContent} className="link">{ option.title }<FontAwesomeIcon className="icon" icon={faArrowUp} /></span></li> )}  
+            </ul>
+          </div>
         </div>
-      </div>
+      </MenuView>
     )
   }
 }
